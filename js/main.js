e3d11b0879884cddb61184f4e14f79435c355895
GuoLiangDai <dgldai@cn.ibm.com>
2018-04-26 09:31:40 +0800

    添加百度统计

--- js/main.js
+++ js/main.js
@@ -217,3 +217,10 @@ var Main = (function (ev) {
 
 );
 var main = new Main;
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?3d8034bf395fa5018d151dc3d2923cb4";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
