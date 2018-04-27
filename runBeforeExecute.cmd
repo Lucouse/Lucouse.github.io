8de6babfe931201c76bb6230335be110dec9e16e
GuoLiangDai <dgldai@cn.ibm.com>
2018-04-27 22:19:31 +0800

    更新项目信息

--- runBeforeExecute.cmd
+++ runBeforeExecute.cmd
@@ -1,11 +1,11 @@
 ::Merge file starting
 @echo off
-copy /b js\api.js+js\utils.js+js\help.js+js\main.js+localComponent\cover\cover.js+localComponent\message\message.js+localComponent\popUp\popUp.js+localComponent\fontFaceIcon\fontFaceIcon.js+js\baiduTongji.js main.js
+copy /b localComponent\footer\footer.js+localComponent\header\header.js+js\utils.js+js\api.js+js\help.js+js\main.js+localComponent\cover\cover.js+localComponent\message\message.js+localComponent\popUp\popUp.js+localComponent\fontFaceIcon\fontFaceIcon.js+js\baiduTongji.js main.js
 @echo off
 echo -----------javaScript Merge completed
-copy /b css\main.css+localComponent\cover\cover.css+localComponent\message\message.css+localComponent\popUp\popUp.css+localComponent\fontFaceIcon\fontFaceIcon.css main.css
+copy /b css\main.css+localComponent\cover\cover.css+localComponent\message\message.css+localComponent\popUp\popUp.css+localComponent\fontFaceIcon\fontFaceIcon.css+localComponent\footer\footer.css+localComponent\header\header.css main.css
 @echo off
-echo -----------javaScript replace completed
+echo -----------css replace completed
 ::compress file starting
 java -jar C:\Users\GuoLiangDai\Desktop\tool\closure-compiler-v20180402.jar main.js --js_output_file main.min.js
 @echo off
