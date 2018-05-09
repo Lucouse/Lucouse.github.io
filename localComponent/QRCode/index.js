25b13bfc5b0bb71203a50683050d9f70c2cfd007
GuoLiangDai <dgldai@cn.ibm.com>
2018-05-09 09:43:55 +0800

    二维码生成器样式修改
    增加二维码颜色，背景色修改。
    调整整体样式

--- localComponent/QRCode/index.js
+++ localComponent/QRCode/index.js
@@ -16,11 +16,27 @@ window.onload = (function (ev) {
      * 生成二维码
      */
     function makeCode() {
+        var container = document.getElementsByClassName("QRCode-inputText-container")[0];
         var elText = document.getElementById("QRCodeInputText");
+        var frontColor = container.getElementsByTagName("ul")[0].getElementsByTagName("li")[0].getElementsByTagName("input")[0].value;
+        if (frontColor === undefined || frontColor === "" || frontColor === null)
+            frontColor = "#000000";
+
+        var backgroundColor = container.getElementsByTagName("ul")[0].getElementsByTagName("li")[0].getElementsByTagName("input")[1].value;
+        if (backgroundColor === undefined || backgroundColor === "" || backgroundColor === null)
+            backgroundColor = "#ffffff";
+        console.log(frontColor + "," + backgroundColor);
         if (!elText.value) {
             elText.focus();
             return;
         }
+        qrcode.clear();
+        qrcode = new QRCode(document.getElementById("QRCode"), {
+            width: 100,
+            height: 100,
+            colorDark:frontColor,
+            colorLight:backgroundColor
+        });
         qrcode.makeCode(elText.value);
     }
 
@@ -34,16 +50,34 @@ window.onload = (function (ev) {
             makeCode();
         }
     });
+
     function buildQRCodePanel(ParentNode) {
         var container = document.createElement("div");
+        var QRCodeInputTextContainer = document.createElement("div");
         var QRCodeInputText = document.createElement("textarea");
+        var ul = document.createElement("ul");
+        var li = document.createElement("li");
+        var inputFrontColor = document.createElement("input");
+        var backgroundColor = document.createElement("input");
         var QRCode = document.createElement("div");
-        container.appendChild(QRCodeInputText)
+        QRCodeInputTextContainer.appendChild(QRCodeInputText);
+        QRCodeInputTextContainer.appendChild(ul);
+        container.appendChild(QRCodeInputTextContainer);
         container.appendChild(QRCode);
         ParentNode.appendChild(container);
-        container.setAttribute("class","QRCode-container");
-        QRCode.setAttribute("id","QRCode");
-        QRCodeInputText.setAttribute("id","QRCodeInputText");
-        QRCodeInputText.setAttribute("placeholder","输入你想生成二维码的文本");
+        container.setAttribute("class", "QRCode-container");
+        QRCode.setAttribute("id", "QRCode");
+        QRCodeInputText.setAttribute("id", "QRCodeInputText");
+        QRCodeInputText.setAttribute("placeholder", "输入你想生成二维码的文本");
+        QRCodeInputTextContainer.setAttribute("class", "QRCode-inputText-container");
+        ul.setAttribute("class", "list-Landscape");
+        inputFrontColor.setAttribute("type", "text");
+        inputFrontColor.setAttribute("placeHolder", "16进制");
+        backgroundColor.setAttribute("type", "text");
+        backgroundColor.setAttribute("placeHolder", "16进制");
+
+        ul.appendChild(li);
+        li.appendChild(inputFrontColor);
+        li.appendChild(backgroundColor);
     }
 });
